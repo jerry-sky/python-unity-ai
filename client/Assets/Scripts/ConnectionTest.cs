86b3ecc8cbdd809cc1c0b6acfdef70db4a2f3c08
Qarian <qarian25@gmail.com>
2020-05-04 15:29:00 +0200

    Succesfully send requests from Unity

---
+++ client/Assets/Scripts/ConnectionTest.cs
@@ -0,0 +1,98 @@
+﻿using System.Text;
+using System.Threading.Tasks;
+//Request library
+using System.Net;
+using System.IO;
+using UnityEngine;
+using TMPro;
+
+public class ConnectionTest : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI textField = default;
+
+
+    public void GetSimpleText()
+    {
+        // localhost:5000 works much more slowly
+        textField.text = Get("http://127.0.0.1:5000/");
+    }
+
+    public void GetData()
+    {
+        textField.text = Get("http://127.0.0.1:5000/data");
+    }
+
+    #region Requests
+    private string Get(string uri)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using(Stream stream = response.GetResponseStream())
+        using(StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    private async Task<string> GetAsync(string uri)
+    {
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+
+        using(HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+        using(Stream stream = response.GetResponseStream())
+        using(StreamReader reader = new StreamReader(stream))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+
+    private string Post(string uri, string data, string contentType, string method = "POST")
+    {
+        byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+        request.ContentLength = dataBytes.Length;
+        request.ContentType = contentType;
+        request.Method = method;
+
+        using(Stream requestBody = request.GetRequestStream())
+        {
+            requestBody.Write(dataBytes, 0, dataBytes.Length);
+        }
+
+        using(HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+        using(Stream stream = response.GetResponseStream())
+        using(StreamReader reader = new StreamReader(stream))
+        {
+            return reader.ReadToEnd();
+        }
+    }
+
+    private async Task<string> PostAsync(string uri, string data, string contentType, string method = "POST")
+    {
+        byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+
+        HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri);
+        request.AutomaticDecompression = DecompressionMethods.GZip | DecompressionMethods.Deflate;
+        request.ContentLength = dataBytes.Length;
+        request.ContentType = contentType;
+        request.Method = method;
+
+        using(Stream requestBody = request.GetRequestStream())
+        {
+            await requestBody.WriteAsync(dataBytes, 0, dataBytes.Length);
+        }
+
+        using(HttpWebResponse response = (HttpWebResponse)await request.GetResponseAsync())
+        using(Stream stream = response.GetResponseStream())
+        using(StreamReader reader = new StreamReader(stream))
+        {
+            return await reader.ReadToEndAsync();
+        }
+    }
+    #endregion /Requests
+}
