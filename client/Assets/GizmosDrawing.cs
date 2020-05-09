1f3aa7a2839a8bc619adc1b39430561abc9b99c1
Necr0o0 <radekklaser@gmail.com>
2020-05-09 23:27:26 +0200

    Base grid generation and movement

---
+++ client/Assets/GizmosDrawing.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GizmosDrawing : MonoBehaviour
+{
+    public GridGenerator grid;
+    private void OnDrawGizmos()
+    {
+        Vector3 currentPos = grid.currentPos;
+        Vector3 startPos = grid.startPos;
+
+        startPos = transform.position;
+        currentPos = grid.startPos;
+
+        for (int i = 0; i < grid.hight; i++)
+        {
+            for (int j = 0; j < grid.width; j++)
+            {
+                Gizmos.DrawCube(currentPos,new Vector3(1,0.2f,1));
+                currentPos += Vector3.right *1.5f;
+            }
+
+            currentPos.x = startPos.x;
+            currentPos += Vector3.forward *1.5f;
+
+        }
+    }
+}
---
+++ client/Assets/MultipleTargetCamera.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultipleTargetCamera : MonoBehaviour
+{
+    public GridGenerator GridGenerator;
+    public List<Transform> targets;
+    public Vector3 offset = new Vector3(0,0,-30f);
+
+    private void Start()
+    {
+        for (int i = 0; i < GridGenerator.hight; i++)
+        {
+            for (int j = 0; j < GridGenerator.width; j++)
+            {
+                targets.Add(GridGenerator.GetGrid(i,j).transform);
+            }
+        }
+    }
+
+    private void LateUpdate()
+    {
+        if(targets.Count == 0)
+            return;
+        Vector3 centerPoint = GetCentrerPoint();
+        Vector3 newPosition = centerPoint +offset;
+        Camera.main.transform.position = newPosition;
+    }
+
+    public Vector3 GetCentrerPoint()
+    {
+        var bounds = new Bounds(targets[0].position,Vector3.zero);
+        for (int i = 0; i < targets.Count; i++)
+        {
+            bounds.Encapsulate(targets[i].position);
+        }
+        return bounds.center;
+    }
+}
