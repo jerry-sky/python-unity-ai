9f37944ed97587f361f4d6a74dc91edd8b89afdf
jerry-sky <jwroczynski@gmail.com>
2020-05-23 13:53:00 +0200

    api: fixed websocket issues related to data formatting and data transmission

--- api/main.py
+++ api/main.py
@@ -2,7 +2,6 @@
 import flask
 from flask import jsonify
 from flask_socketio import SocketIO, Namespace, emit
-import json
 
 # flask setup
 app = flask.Flask(__name__)
@@ -15,7 +14,7 @@ socketio = SocketIO(app, cors_allowed_origins='*')
 # standard flask routes
 @app.route('/', methods=['GET'])
 def root():
-    return "<i>it</i> <b>works!</b>"
+    return '<i>it</i> <b>works!</b>'
 
 
 @app.route('/data', methods=['GET'])
@@ -25,19 +24,6 @@ def more_data():
     })
 
 # socket.io event listeners
-class TestingNamespace(Namespace):
-
-    def on_connect(self):
-        pass
-
-    def on_disconnect(self):
-        pass
-
-    def on_my_event(self, data):
-        print('--- MY EVENT OCCURRED ---')
-        emit('my_response', {'data': 'yup, you\'re connected'})
-
-
 class SimulationNamespace(Namespace):
 
     def on_connect(self):
@@ -46,16 +32,15 @@ class SimulationNamespace(Namespace):
     def on_disconnect(self):
         pass
 
-    def on_start(self, data):
-        print("received")
-        # can't receive " character from Unity
-        data = data.replace("\'", "\"")
-        data = json.loads(data)
-        print(data)
-        emit('update', {'rabbits': [{'alive': True, 'x': 3, 'y': 5}, {'alive': False, 'x': 1, 'y': 2}], 'wolfs': [{'alive': True, 'x': 1, 'y': 2}, {'alive': False, 'x': 3, 'y': 4}]})
+    def on_start(self, data=None):
+        if data is not None:
+            print('received:', data)
+        # testing measure – TODO: delete @Qarian
+        emit('update', {'rabbits': [{'alive': True, 'x': 3, 'y': 5}, {'alive': False, 'x': 1, 'y': 2}], 'wolfs': [
+             {'alive': True, 'x': 1, 'y': 2}, {'alive': False, 'x': 3, 'y': 4}]})
 
 
 socketio.on_namespace(SimulationNamespace('/simulation'))
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     socketio.run(app)
