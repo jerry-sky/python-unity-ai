b0d11d200cb5946e8cea9d5dd2247c0c7c519ce4
Necr0o0 <radekklaser@gmail.com>
2020-06-03 14:31:40 +0200

    unity: added rabbit and wolf models (mock-up)

---
+++ client/Assets/Resources/Scripts/GameManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private int hight;
+    private int width;
+    private GameObject[,] gridArray;
+    public static GameManager gm;
+
+    private ServerCommunication _serverCommunication;
+
+    public void Awake()
+    {
+        gm = this;
+        _serverCommunication = transform.GetComponent<ServerCommunication>();
+        _serverCommunication.simulationNamespace.onUpdate.AddListener(ResponseFromServer);
+    }
+
+    public void StartSimulation()
+    {
+        hight = GridGenerator.Instance.height;
+        width = GridGenerator.Instance.width;
+        gridArray = GridGenerator.Instance.gridArray;
+
+    }
+
+    public void ResponseFromServer( BoardUpdateModel data)
+    {
+        for (int i = 0; i < data.wolves.Length; i++)
+        {
+            if(data.wolves[i].alive)
+                SetObjectPosition(GridGenerator.Instance.wolvesList[i].transform, data.wolves[i].x, data.wolves[i].y);
+            else
+            {
+                GridGenerator.Instance.wolvesList[i].SetActive(false);
+            }
+        }
+        for (int i = 0; i < data.rabbits.Length; i++)
+        {
+            if(data.rabbits[i].alive)
+                SetObjectPosition(GridGenerator.Instance.rabbitsList[i].transform, data.rabbits[i].x, data.rabbits[i].y);
+            else
+            {
+                GridGenerator.Instance.rabbitsList[i].SetActive(false);
+            }
+        }
+    }
+
+    public Vector2 SetObjectPosition(Transform gameObject, int x, int y)
+    {
+        x = (x + hight) % hight;
+        y = (y + width) % width;
+
+        GridGenerator.Instance.SetColorAtPosition(x,y);
+        var pos = gridArray[x, y].transform.position;
+        pos.y += 1f;
+        gameObject.DOMove(pos, 0.5f);
+
+        return new Vector2(x,y);
+    }
+}
---
+++ client/Assets/Resources/Scripts/SimulationConfirm.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class SimulationConfirm : MonoBehaviour
+{
+    public TMP_InputField wolvesTXT,rabbitsTXT,heightTXT,widthTXT;
+    public GameObject canvas;
+    public CameraMovement cameraMove;
+
+    [SerializeField] private ServerCommunication _serverCommunication = default;
+
+    IEnumerator CheckServer()
+    {
+        while (true)
+        {
+            _serverCommunication.simulationNamespace.CheckUpdate();
+            yield return new WaitForSeconds(1);
+        }
+    }
+
+    public void ButtonClicked()
+    {
+        SimulationDataModel data = new SimulationDataModel()
+        {
+            height = Int32.Parse(heightTXT.text),
+            width = Int32.Parse(widthTXT.text),
+            wolves_count = Int32.Parse(wolvesTXT.text),
+            rabbits_count = Int32.Parse(rabbitsTXT.text)
+        };
+        _serverCommunication.simulationNamespace.StartSimulation(data);
+        canvas.SetActive(false);
+        cameraMove.enabled = true;
+        GridGenerator.Instance.SettingsConfirmed(data.width,data.height,data.wolves_count,data.rabbits_count);
+        StartCoroutine(CheckServer());
+    }
+}
